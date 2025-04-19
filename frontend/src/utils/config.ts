652db83e1e1efd239fea40185c3bf447d7986d3c
blackcave0 <sarvesh.sri00@gmail.com>
2025-04-19 09:02:58 +0530

    Update API and Socket.IO URLs in config.ts to new IP address for improved connectivity.

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,8 +1,8 @@
 // API URL - 10.0.2.2 for Android emulator, localhost for web, or your machine's IP for physical devices
-export const API_URL = 'http://192.168.215.181:5000';
+export const API_URL = 'http://192.168.232.181:5000';
 
 // Socket.IO URL
-export const SOCKET_URL = 'http://192.168.215.181:5000';
+export const SOCKET_URL = 'http://192.168.232.181:5000';
 
 // Default avatar
 export const DEFAULT_AVATAR = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
